d9fd0de337f051286a8d023e8edb57021021d69b
griphly1 <griphly11@gmail.com>
2023-02-27 07:37:28 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev  imprime en reversa
+ * print_rev - imprime en reversa
  * @s: string
  * return: 0
  */
@@ -15,7 +15,7 @@ void print_rev(char *s)
 		s++;
 	}
 	s--;
-	for (0 = longi o > 0; o--)
+	for (o = longi o > 0; o--)
 {
 	_putchar(*s);
 	s--;
